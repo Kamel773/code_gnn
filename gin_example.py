97f18bc4ffc8720f505e9a783c0e171e6282f58e
Benjamin Steenhoek <benjaminjsteenhoek@gmail.com>
2021-09-26 19:14:59 -0500

    Compare with GINWithJK. Also fix pesky bug with lr_scheduler.step().

--- gin_example.py
+++ gin_example.py
@@ -1,20 +1,24 @@
+import os
+import random
+import shutil
+
 import numpy as np
 import torch
 from torch import nn
-from torch.nn import functional as F, CrossEntropyLoss, Sequential, Linear, ReLU, BatchNorm1d
+from torch.nn import functional as F, CrossEntropyLoss, Sequential, Linear, ReLU, BatchNorm1d, NLLLoss
 from torch.optim import Adam
 from torch.optim.lr_scheduler import StepLR
 from torch.utils.data import random_split
 from torch.utils.tensorboard import SummaryWriter
 from torch_geometric.datasets import TUDataset
 from torch_geometric.loader import DataLoader
-from torch_geometric.nn import GINConv, JumpingKnowledge, global_mean_pool
+from torch_geometric.nn import GINConv, JumpingKnowledge, global_mean_pool, global_add_pool
 from torch_geometric.utils import to_networkx
 import networkx as nx
 from matplotlib import pyplot as plt
 import itertools
 
-from torch_scatter import scatter_mean
+from pytorch_geometric.benchmark.kernel.gin import GINWithJK
 
 
 def draw_9_plots(dataset):
@@ -34,6 +38,10 @@ def draw_9_plots(dataset):
 class MyGIN(nn.Module):
     """
     Adapted from pytorch_geometric/benchmark/kernel/gin.py
+
+    pyg implements the node embeddings in GINConv but the readout is pretty customizable, so it's in this module.
+    The basic version in basic_gnn.py does not handle minibatches, so I adapted this implementation instead.
+    The benchmark version uses mean pooling though the paper uses sum pooling, so I replaced it.
     """
     def __init__(self, dataset, num_layers, hidden_features):
         super().__init__()
@@ -52,18 +60,18 @@ class MyGIN(nn.Module):
         self.lin1.reset_parameters()
         self.lin2.reset_parameters()
 
-    def forward(self, x, edge_index, batch):
-        layer_hs = []
-        node_h = x
+    def forward(self, data):
+        x, edge_index, batch = data.x, data.edge_index, data.batch
+        xs = []
         for conv in self.convs:
-            node_h = conv(node_h, edge_index)
-            layer_hs.append(node_h)
-        h_concat = self.jump(layer_hs)  # JumpingKnowledge('cat') just concatenates node features from all layers
-        graph_h = global_mean_pool(h_concat, batch)  # Readout from each graph in the batch
-        graph_h = F.relu(self.lin1(graph_h))
-        graph_h = F.dropout(graph_h, p=0.5, training=self.training)
-        graph_h = self.lin2(graph_h)
-        return graph_h
+            x = conv(x, edge_index)
+            xs.append(x)
+        x = self.jump(xs)  # JumpingKnowledge('cat') just concatenates node features from all layers
+        x = global_mean_pool(x, batch)  # Readout from each graph in the batch
+        x = F.relu(self.lin1(x))
+        x = F.dropout(x, p=0.5, training=self.training)
+        x = self.lin2(x)
+        return F.log_softmax(x, dim=-1)
 
     @staticmethod
     def MLP(in_features, hidden):
@@ -76,21 +84,24 @@ class MyGIN(nn.Module):
         )
 
 
-def measure_performance(model, device, test_loader):
+def measure_performance(model, device, test_loader, loss_fn):
     model.eval()
     with torch.no_grad():
         all_correct = 0
         epoch_size = 0
+        losses = []
         for data in test_loader:
-            x, edge_index, batch = data.x.to(device), data.edge_index.to(device), data.batch.to(device)
-            label = data.y.float().to(device)
-            out = model(x, edge_index, batch)
+            data = data.to(device)
+            label = data.y.to(device)
+            out = model(data)
+            loss = loss_fn(out, label.view(-1))
+            losses.append(loss.item() * len(out))
             pred = torch.argmax(out, dim=1)
             correct = pred == label
             all_correct += correct.sum().item()
             epoch_size += len(label)
         acc = all_correct / epoch_size
-        return acc
+        return acc, sum(losses) / len(test_loader.dataset)
 
 
 def test_agg_concat():
@@ -111,20 +122,22 @@ def test_agg_concat():
 
         # agg(concat(h_i for h_i in h))
         h_cat = torch.cat((h1, h2, h3), dim=1)
-        h_cat_pool = global_mean_pool(h_cat, batch)
+        h_cat_pool = global_add_pool(h_cat, batch)
 
         # concat(agg(h_i) for h_i in h)
-        h1_pool = global_mean_pool(h1, batch)
-        h2_pool = global_mean_pool(h2, batch)
-        h3_pool = global_mean_pool(h3, batch)
+        h1_pool = global_add_pool(h1, batch)
+        h2_pool = global_add_pool(h2, batch)
+        h3_pool = global_add_pool(h3, batch)
         h_pool_cat = torch.cat((h1_pool, h2_pool, h3_pool), dim=1)
 
         assert torch.equal(h_cat_pool, h_pool_cat), 'concat/agg are not commutative??'
 
 
 def main():
-    np.random.seed(0)
-    torch.random.manual_seed(0)
+    seed = 0
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.random.manual_seed(seed)
     dataset = TUDataset(root='D:/datasets/TUDataset', name='PROTEINS')
 
     device = torch.device('cuda')
@@ -133,52 +146,48 @@ def main():
     splits = [n_train, len(dataset) - n_train]
     print('Splits:', splits[0], 'train', splits[1], 'test')
     train_set, test_set = random_split(dataset, splits)
-    train_loader = DataLoader(train_set, batch_size=32, shuffle=True)
-    test_loader = DataLoader(test_set, batch_size=32)
+    train_loader = DataLoader(train_set, batch_size=128, shuffle=True)
+    test_loader = DataLoader(test_set, batch_size=128)
 
-    data = next(iter(train_loader))
-
-    model = MyGIN(dataset, num_layers=5, hidden_features=64).to(device)
+    num_layers = 5
+    hidden_features = 64
+    model = MyGIN(dataset, num_layers=num_layers, hidden_features=hidden_features).to(device)
+    # model = GINWithJK(dataset, num_layers, hidden_features).to(device)
     model.reset_parameters()
     print(list((p.shape for p in model.parameters())))
-    # I'm finding that the GIN model does not handle pyg minibatches
-    # model = GIN(in_channels=3, hidden_channels=64, num_layers=5, out_channels=2, jk='cat').to(device)
-    loss_fn = CrossEntropyLoss()
-    optimizer = Adam(model.parameters(), lr=0.01)
-    scheduler = StepLR(optimizer, step_size=50, gamma=0.5)
+    loss_fn = NLLLoss()
+    optimizer = Adam(model.parameters(), lr=0.01, weight_decay=0)
+    scheduler = StepLR(optimizer, gamma=0.5, step_size=50)
     log_every = 25
-    tb = SummaryWriter()
+    tb_dir = 'runs/' + '-'.join(str(s) for s in (dataset.name, type(model).__name__, num_layers, hidden_features))
+    if os.path.exists(tb_dir):
+        shutil.rmtree(tb_dir)
+    tb = SummaryWriter(tb_dir)
     for i in range(400):
-        epoch_loss = []
-        all_correct = 0
-        epoch_size = 0
+        epoch_loss = 0
         # torch_geometric.loader.DataLoader concatenates all the graphs in the batch
         # into one big disjoint graph, so we can train with a batch as if it's a single graph.
         model.train()
-        for data in train_loader:
+        for j, data in enumerate(train_loader):
             optimizer.zero_grad()
-            x, edge_index, batch = data.x.to(device), data.edge_index.to(device), data.batch.to(device)
-            label = data.y.to(device)
-            out = model(x, edge_index, batch)
-            loss = loss_fn(out, label)
+            data = data.to(device)
+            out = model(data)
+            loss = loss_fn(out, data.y.view(-1))
             loss.backward()
+            epoch_loss += loss.item() * data.num_graphs
             optimizer.step()
-            scheduler.step()
-            epoch_loss.append(loss.detach().item())
-            with torch.no_grad():
-                pred = torch.argmax(out, dim=1)
-                correct = pred == label
-                all_correct += correct.float().sum().item()
-                epoch_size += len(label)
-        train_acc = all_correct / epoch_size
-        train_loss = sum(epoch_loss)/len(epoch_loss)
-        tb.add_scalar('acc/train', train_acc, i)
-        tb.add_scalar('loss/train', train_loss, i)
+        train_loss = epoch_loss / len(train_loader.dataset)
+        train_acc, _ = measure_performance(model, device, train_loader, loss_fn)
+        tb.add_scalar('train_acc', train_acc, i)
+        tb.add_scalar('train_loss', train_loss, i)
+        test_acc, test_loss = measure_performance(model, device, test_loader, loss_fn)
+        tb.add_scalar('test_acc', test_acc, i)
+        tb.add_scalar('val_loss', test_loss, i)
         if i % log_every == 0:
-            acc = measure_performance(model, device, test_loader)
-            tb.add_scalar('acc/test', acc, i)
             # print('Epoch:', i, 'Train Accuracy:', train_acc, 'Train loss:', train_loss, 'Test Accuracy:', acc)
-            print('Epoch:', i, 'Train loss:', train_loss, 'Test Accuracy:', acc)
+            print('Epoch:', i, 'Train loss:', train_loss, 'Test Accuracy:', test_acc)
+        scheduler.step()
+
 
 
 if __name__ == '__main__':
