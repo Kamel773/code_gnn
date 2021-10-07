9d507251795f3d2527ead3545065515b70540fa0
Benjamin Steenhoek <benjaminjsteenhoek@gmail.com>
2021-10-07 13:38:14 -0500

    Add edge type as edge attribute

--- data.py
+++ data.py
@@ -93,8 +93,8 @@ def get_codebert_embedding(file, cpg, codebert_api):
     return embedding
 
 
-# Source: https://github.com/VulDetProject/ReVeal/blob/ca31b783384b4cdb09b69950e48f79fa0748ef1d/data_processing/create_ggnn_data.py#L225-L242
-type_map = {
+# Source: https://github.com/VulDetProject/ReVeal/blob/ca31b783384b4cdb09b69950e48f79fa0748ef1d/data_processing/create_ggnn_data.py#L225-L257
+node_type_map = {
     'AndExpression': 1, 'Sizeof': 2, 'Identifier': 3, 'ForInit': 4, 'ReturnStatement': 5, 'SizeofOperand': 6,
     'InclusiveOrExpression': 7, 'PtrMemberAccess': 8, 'AssignmentExpression': 9, 'ParameterList': 10,
     'IdentifierDeclType': 11, 'SizeofExpression': 12, 'SwitchStatement': 13, 'IncDec': 14, 'Function': 15,
@@ -111,7 +111,22 @@ type_map = {
     'ContinueStatement': 64, 'MemberAccess': 65, 'ExpressionStatement': 66, 'ForStatement': 67, 'InitializerList': 68,
     'ElseStatement': 69
 }
-type_one_hot = torch.eye(len(type_map))
+type_one_hot = torch.eye(len(node_type_map))
+# We currently consider 12 types of edges mentioned in ICST paper
+edge_type_map = {
+    'IS_AST_PARENT': 1,
+    'IS_CLASS_OF': 2,
+    'FLOWS_TO': 3,
+    'DEF': 4,
+    'USE': 5,
+    'REACHES': 6,
+    'CONTROLS': 7,
+    'DECLARES': 8,
+    'DOM': 9,
+    'POST_DOM': 10,
+    'IS_FUNCTION_OF_AST': 11,
+    'IS_FUNCTION_OF_CFG': 12
+}
 
 
 def get_word2vec_embedding(cpg, wv_model):
@@ -168,19 +183,19 @@ def get_dataset(use_codebert=False, use_word2vec=False):
 
         # Concatenate node type
         node_type = nx.get_node_attributes(cpg, 'type')
-        type_embeddings = torch.stack([type_one_hot[type_map[node_type[node]] - 1] for node in cpg.nodes])
+        type_embeddings = torch.stack([type_one_hot[node_type_map[node_type[node]] - 1] for node in cpg.nodes])
         node_embeddings = torch.cat((type_embeddings, node_embeddings), dim=1)
 
         data['node'].x = node_embeddings
 
-        num_features_edge = 128
+        edge_type = nx.get_edge_attributes(cpg, 'type')
         edges_by_type = defaultdict(list)
         for u, v, t in cpg.edges.data("type"):
             edges_by_type[t].append((u, v))
         for t, edges in edges_by_type.items():
             edge_data = data['node', t, 'node']
             edge_data.edge_index = torch.tensor(edges)
-            edge_data.edge_attr = torch.randn((len(edges), num_features_edge))
+            edge_data.edge_attr = torch.stack([torch.tensor([edge_type_map[edge_type[edge]]]) for edge in cpg.edges if edge_type[edge] != 'IS_FILE_OF'], dim=0)
         data_list.append(data)
 
     data = data_list[0]
