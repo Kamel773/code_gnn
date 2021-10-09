d8e979ff02e749ea93a8a6fa5eb30353d5584bb4
Benjamin Steenhoek <benjaminjsteenhoek@gmail.com>
2021-10-08 20:25:34 -0500

    Add docker command

--- run_docker.bat
+++ run_docker.bat
@@ -1,2 +1,3 @@
 docker build -t joern:latest .
-docker run -vD:\weile-lab\thesis\ssl-gnn\joern:/share -p 127.0.0.1:8080:8080 --name joern_server -itd joern:latest bash
+docker run -vD:\weile-lab\thesis\code_gnn:/share --name joern -itd joern:latest bash
+docker run -vD:\weile-lab\thesis\code_gnn:/share -p 8080:8080 --name joern_server -itd joern:latest /joern/joern --server --server-host 0.0.0.0 --server-port 8080
